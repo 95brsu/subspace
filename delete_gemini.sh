54f19da6cb6df67c7753b5cfbace3466140ee718
95brsu <103598894+95brsu@users.noreply.github.com>
2022-06-11 12:13:34 +0300

    Create delete_gemini.sh

---
+++ delete_gemini.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo "=================================================="
+echo -e "\033[0;35m"
+echo " ::::    :::  ::     ::  :::::::::   ::::::::   ";
+echo " :+:+:   :+: :+:    :+:     :+:     :+:    :+:  ";
+echo " :+:+:+  +:+ +:+    +:+     +:+     +:+         ";
+echo " +#+ +:+ +#+ +#+    +:+     +:+     +#++:++#++  ";
+echo " +#+  +#+#+# +#+    +#+     +#+             +#+ ";
+echo " #+#   #+#+# #+#    #+#     #+#     #+#     #+# ";
+echo " ###    ####  ########    ######    ########    ";
+echo -e "\e[0m"
+echo "=================================================="
+
+sleep 2
+
+function line {
+  echo "----13.06.2022--------------------------------------------------------------------"
+}
+
+function colors {
+  GREEN="\e[1m\e[32m"
+  RED="\e[1m\e[39m"
+  NORMAL="\e[0m"
+}
+
+
+function delete {
+
+sudo systemctl stop subspaced subspaced-farmer
+sudo systemctl disable subspaced subspaced-farmer
+rm -rf ~/.local/share/subspace*
+rm -rf /etc/systemd/system/subspaced*
+rm -rf /usr/local/bin/subspace*
+}
+
+
+
+colors
+line
+echo -e "${RED}Удаляем subspace${NORMAL}"
+delete
+line
+echo -e "${RED}Скрипт завершил свою работу!!!Извините за внимание! Удаление успешно завершено! ${NORMAL}"
