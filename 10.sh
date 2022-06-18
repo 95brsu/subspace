85021054d7b5221e56d7c1237e5f4533c7407f25
95brsu <103598894+95brsu@users.noreply.github.com>
2022-06-18 23:49:33 +0300

    Update 10.sh

--- 10.sh
+++ 10.sh
@@ -30,8 +30,8 @@ function update {
 cd $HOME
 sudo apt update && sudo apt install ocl-icd-opencl-dev libopencl-clang-dev -y
 systemctl stop subspaced subspaced-farmer
-wget -O subspace-node https://github.com/subspace/subspace/releases/download/gemini-1b-2022-jun-18/subspace-node-ubuntu-x86_64-gemini-1b-2022-jun-18
-wget -O subspace-farmer https://github.com/subspace/subspace/releases/download/gemini-1b-2022-jun-18/subspace-farmer-ubuntu-x86_64-gemini-1b-2022-jun-18
+wget -O subspace-node https://github.com/subspace/subspace/releases/download/gemini-1b-2022-jun-13/subspace-node-ubuntu-x86_64-gemini-1b-2022-jun-13
+wget -O subspace-farmer https://github.com/subspace/subspace/releases/download/gemini-1b-2022-jun-13/subspace-farmer-ubuntu-x86_64-gemini-1b-2022-jun-13
 chmod +x subspace-node
 chmod +x subspace-farmer
 mv subspace-node /usr/local/bin/
